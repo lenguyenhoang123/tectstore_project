df0e3b9db1e1fab8aaa2db8e37cf7397c66f94ed
lenguyenhoang123 <lenguyenhoang9.10@gmail.com>
2026-05-15 21:06:23 +0700

    fix db

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Kết nối MongoDB (dùng tên database theo tên dự án: techstore)
-const mongoUri = 'mongodb+srv://tectstore:<tectstore>@cluster0.jnowrmt.mongodb.net/?appName=Cluster0';
+const mongoUri = process.env.MONGODB_URI || 'MONGO_URI=mongodb+srv://tectstore:tectstore@cluster0.jnowrmt.mongodb.net/techstore?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
